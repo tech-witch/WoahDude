8145c90faa7f5f22571bb93a74ef9cb23bf0ba6d
tech-witch <lianedecarychen@gmail.com>
2018-07-30 12:51:24 -0400

    Comments added

--- particleSystem.js
+++ particleSystem.js
@@ -1,10 +1,8 @@
-
-
+//Series of particle systems using different types of particles
 
 //---------PARTICLE CLASSES----------------
 
-
-//Base code taken from "Coding Challenge #78: Simple Particle System" at https://www.youtube.com/watch?v=UcdigVaIYAk
+//Base code adapted from "Coding Challenge #78: Simple Particle System" at https://www.youtube.com/watch?v=UcdigVaIYAk
 class Particle{
 
   constructor(origin, velocity, size){
@@ -68,46 +66,42 @@ class Particle{
     this.vy=this.vy*velocity;
     this.vz=this.vz*velocity;
   }
-
-
-
 }
 
 
+//First type of particle: generates particles from center, they go in all directions and reduce in size.
+// "Bubbling" effect
 class regularParticle extends Particle{
-
-
   show(){
     push()
     specularMaterial(this.myColor,this.myColor,this.myColor);
     translate(this.x,this.y,this.z);
     noStroke();
-  // rotate(this.xRotation);
     sphere(this.mySize);
     pop();
   }
 
   update(){
+    //Move particle (all directions) and make it smaller.
     this.updatePosition(this.vx, this.vy, this.vz);
     this.reduceSize();
   }
+
   reduceSize(){
-      if(this.mySize>0){
+      if(this.mySize>0){ //If its size is above 0, make smaller
         this.mySize-=1;
       }
   }
-
 }
 
+//Same as regularParticle, but reduces its size much faster.
+//sun-like effect: something is simering at its surface
 class sunParticle extends Particle{
-
-
   show(){
     push()
     specularMaterial(this.myColor,this.myColor,this.myColor);
     translate(this.x,this.y,this.z);
     noStroke();
-  // rotate(this.xRotation);
     sphere(this.mySize);
     pop();
   }
@@ -116,23 +110,22 @@ class sunParticle extends Particle{
     this.updatePosition(this.vx, this.vy, this.vz);
     this.reduceSize();
   }
+
   reduceSize(){
       if(this.mySize>0){
-        this.mySize-=4;
+        this.mySize-=4; //Reduce by a larger increment then the regularParticle
       }
   }
-
 }
 
+
+//Same as regularParticle, but it creates toruses instead of spheres
 class torusParticle extends Particle{
-
-
   show(){
     push()
     specularMaterial(this.myColor,this.myColor,this.myColor);
     translate(this.x,this.y,this.z);
     noStroke();
-  // rotate(this.xRotation);
     torus(this.mySize);
     pop();
   }
@@ -141,18 +134,16 @@ class torusParticle extends Particle{
     this.updatePosition(this.vx, this.vy, this.vz);
     this.reduceSize();
   }
-
 }
 
+//Same as regularParticle, but reduces paticles slower
+// Creates a spilar effect while spinning
 class windParticle extends Particle{
-
-
   show(){
     push()
     specularMaterial(this.myColor,this.myColor,this.myColor);
     translate(this.x,this.y,this.z);
     noStroke();
-  // rotate(this.xRotation);
     sphere(this.mySize);
     pop();
   }
@@ -162,7 +153,7 @@ class windParticle extends Particle{
     this.reduceSize();
   }
   reduceSize(){
-    if (counter==3){
+    if (counter==3){ //The counter makes the particles dissapear slower, change value to change speed
       counter=0;
       if(this.mySize>0){
         this.mySize-=1;
@@ -184,34 +175,34 @@ class windParticle extends Particle{
 //---------PARTICLE SYSTEM CLASSES----------------
 
 
-
+//Basic particle system using regular particles. "bubbling" effect
 class ParticleSystem{
   constructor(){
     this.particles =[];
-    this.addParticles(10);
+    this.addParticles(10); //Changed the intensity of the bubbling
   }
 
   addParticles(val){
     for (var i=0;i<val;i++){
-      let p=new regularParticle(0,5,50);
+      let p=new regularParticle(0,5,50); //Particle of origin 0, velocity 5 and size 50
       this.particles.push(p);
     }
 
   }
 
-
+//Adds new particles, removes particles whose size is bellow 0, updates position and shows particles
   show(){
-
-    this.addParticles(2);
+    this.addParticles(2); //CHanges intensity of bubbling
     for (let i=0; i<this.particles.length; i++){
       if (this.particles[i].finished()){
-            this.particles.splice(i,1);
+            this.particles.splice(i,1); //remove from array when "finished"
       }
       this.particles[i].update();
       this.particles[i].show();
   }
 }
 
+//Changes the speed at which particles scatter by value
   updateParticleScatter(val){
     for (var i = 0; i < this.col; i++){
       for (var j = 0; j < this.row; j++){
@@ -228,39 +219,26 @@ class ParticleSystem{
 
 }
 
-
+//Shows nothing. Used to clear dispay during initialization
 class emptySystem extends ParticleSystem{
 
   addParticles(val){
     for (var i=0;i<val;i++){
-
     }
-
   }
-
-
   show(){
-
-
 }
-
-
 }
 
 
 class sunSystem extends ParticleSystem{
-
   addParticles(val){
     for (var i=0;i<val;i++){
       let p=new sunParticle(0,5,50);
       this.particles.push(p);
     }
-
   }
-
-
   show(){
-
     this.addParticles(10);
     for (let i=0; i<this.particles.length; i++){
       if (this.particles[i].finished()){
@@ -281,6 +259,7 @@ class sunSystem extends ParticleSystem{
   }
 
 }
+
 
 var counter=0;
 class windSystem extends ParticleSystem{
